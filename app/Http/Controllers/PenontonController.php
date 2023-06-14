0e05b47521064bee8e4f637334b08ea0f4793785
fajarhikari <fajarhikari91@gmail.com>
2023-06-14 11:10:43 +0700

    next

---
+++ app/Http/Controllers/PenontonController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penonton;
+use App\Http\Requests\StorePenontonRequest;
+use App\Http\Requests\UpdatePenontonRequest;
+
+class PenontonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePenontonRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Penonton $penonton)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Penonton $penonton)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePenontonRequest $request, Penonton $penonton)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Penonton $penonton)
+    {
+        //
+    }
+}
